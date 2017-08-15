39d302fefab53579e1e3dda09619c34796a27095
w1lm3rgdz <wilmer.guedz@gmail.com>
2017-08-15 16:45:19 -0400

    initial setup
    Firmware development on esp8266 board using MQTT protocol

--- snic-esp_mqtt_client/snic-esp_mqtt_client.ino
+++ snic-esp_mqtt_client/snic-esp_mqtt_client.ino
@@ -1,9 +1,128 @@
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+//Update the network values
+const char* ssid = "";
+const char* password = "";
+const char* mqtt_server = "";
+const char *topic = "";
+const int GPIO4 = 04;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '0') {
+    digitalWrite(GPIO4, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is acive low on the ESP-01)
+  } else {
+    digitalWrite(GPIO4, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+}
+
+//Generate mac number to string
+String macToStr(const uint8_t* mac)
+{
+  String result;
+  for (int i = 0; i < 6; ++i) {
+    result += String(mac[i], 16);
+    if (i < 5)
+      result += ':';
+  }
+  return result;
+}
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
 void setup() {
-  // put your setup code here, to run once:
+  
+  // Initialize the GPIO4 pin as an output
+  pinMode(GPIO4, OUTPUT);     
+
+  Serial.begin(115200);
+
+  delay(10);
+
+  setup_wifi();
+
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void reconnect() {
+// Generate client name based on MAC address and last 8 bits of microsecond counter
+  String clientName;
+  clientName += "esp8266-";
+  uint8_t mac[6];
+  WiFi.macAddress(mac);
+  clientName += macToStr(mac);
+  clientName += "-";
+  clientName += String(micros() & 0xff, 16);
+  
+  // Loop until we're reconnected
+  while (!client.connected()) {
+  
+  Serial.print("Connecting to ");
+  Serial.print(mqtt_server);
+  Serial.print(" as ");
+  Serial.println(clientName);
+  
+    if (client.connect(clientName.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish(topic, "hello world");
+      // ... and resubscribe
+      client.subscribe(topic);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void loop() {  
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  
+  client.loop();
 
 }
